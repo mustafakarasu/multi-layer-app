12f24c02df071c39ed42b20a19a6cd7c5f56d213
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-28 00:02:56 +0300

    Services have been added to the Core layer.

---
+++ MultiLayer.Core/Services/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using MultiLayer.Core.DTOs;
+using MultiLayer.Core.Models;
+
+namespace MultiLayer.Core.Services;
+
+public interface ICategoryService : IService<Category>
+{
+    public Task<CustomResponseDto<CategoryWithProductsDto>> GetCategoryByIdAsync(int categoryId);
+}
---
+++ MultiLayer.Core/Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using MultiLayer.Core.DTOs;
+using MultiLayer.Core.Models;
+
+namespace MultiLayer.Core.Services;
+
+public interface IProductService : IService<Product>
+{
+    Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetAllProducts();
+}
