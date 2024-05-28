3c50d533edfd1be77a14ab1041a5a2c42efb8dff
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-28 18:56:00 +0300

    Repository layer, AppDbContext and entity configurations were created.

---
+++ MultiLayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiLayer.Core.Models;
+
+namespace MultiLayer.Repository.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            
+            builder.HasOne(x => x.Product)
+                .WithOne(x => x.ProductFeature)
+                .HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
