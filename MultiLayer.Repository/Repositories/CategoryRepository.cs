f4d8c7f5801929f5b874640b3ad7d4469857af55
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-29 00:02:44 +0300

    Repository classes were created in the Repository layer.

---
+++ MultiLayer.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiLayer.Core.Models;
+using MultiLayer.Core.Repositories;
+
+namespace MultiLayer.Repository.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int categoryId)
+        {
+            return await Context.Categories
+                .Include(x => x.Products)
+                .Where(x => x.Id == categoryId)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
---
+++ MultiLayer.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiLayer.Core.Models;
+using MultiLayer.Core.Repositories;
+
+namespace MultiLayer.Repository.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await Context.Products.Include(x => x.Category).ToListAsync();
+        }
+    }
+}
