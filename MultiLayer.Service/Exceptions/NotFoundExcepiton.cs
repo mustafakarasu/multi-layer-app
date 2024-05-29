7ff53aea66372407f8d05ae3da44477254698373
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-29 23:12:30 +0300

    Fix: Missing custom exception classes have been added.

---
+++ MultiLayer.Service/Exceptions/NotFoundExcepiton.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiLayer.Service.Exceptions
+{
+    public class NotFoundExcepiton : Exception
+    {
+        public NotFoundExcepiton(string message) : base(message)
+        {
+        }
+    }
+}
