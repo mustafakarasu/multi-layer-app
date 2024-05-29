d056a5c7142bf9c356eea4c042e4344cb2c4c8da
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-29 11:42:59 +0300

    Unit Of Work implementation was made in the repository layer.

---
+++ MultiLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using MultiLayer.Core.UnitOfWorks;
+
+namespace MultiLayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
