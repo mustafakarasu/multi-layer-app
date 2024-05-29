2def071a4bc882df73e96b641928904eef74fd78
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-29 14:02:18 +0300

    Seed data was added to the repository layer.

---
+++ MultiLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiLayer.Core.Models;
+
+namespace MultiLayer.Repository.Seeds
+{
+    public class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData
+            (
+                new Category { Id = 1, Name = "Kalemler" },
+                new Category { Id = 2, Name = "Kitaplar" },
+                new Category { Id = 3, Name = "Defterler" }
+            );
+        }
+    }
+}
