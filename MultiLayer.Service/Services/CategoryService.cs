4af0e6157c1250ae3a1cb4201da44fa56da3f8cb
Mustafa Karasu <mustaffa.karasu@hotmail.com>
2024-05-30 12:10:50 +0300

    Codes were written for service operations in the service layer.

---
+++ MultiLayer.Service/Services/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MultiLayer.Core.DTOs;
+using MultiLayer.Core.Models;
+using MultiLayer.Core.Repositories;
+using MultiLayer.Core.Services;
+using MultiLayer.Core.UnitOfWorks;
+
+namespace MultiLayer.Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(
+            IGenericRepository<Category> repository, 
+            IUnitOfWork unitOfWork, 
+            IMapper mapper, 
+            ICategoryRepository categoryRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<CustomResponseDto<CategoryWithProductsDto>> GetCategoryByIdAsync(int categoryId)
+        {
+            var category = await _categoryRepository.GetCategoryByIdAsync(categoryId);
+            var categoryDto = _mapper.Map<CategoryWithProductsDto>(category);
+
+            return CustomResponseDto<CategoryWithProductsDto>.Success(200, categoryDto);
+        }
+    }
+}
---
+++ MultiLayer.Service/Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MultiLayer.Core.DTOs;
+using MultiLayer.Core.Models;
+using MultiLayer.Core.Repositories;
+using MultiLayer.Core.Services;
+using MultiLayer.Core.UnitOfWorks;
+
+namespace MultiLayer.Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(
+            IGenericRepository<Product> repository, 
+            IUnitOfWork unitOfWork, 
+            IMapper mapper, 
+            IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetAllProducts()
+        {
+            var products = await _productRepository.GetAllProducts();
+            var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
+            
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+        }
+    }
+}
